f8da1dfecedf9c1b2cb89d99fa0e0cd3e7320833
CamiDINET <117636454+CamiDINET@users.noreply.github.com>
2023-07-21 16:04:20 +0200

    start

---
+++ src/main/java/com/shortener/shortener/controller/ShortenerController.java
@@ -0,0 +1,16 @@
+package com.shortener.shortener.controller;
+
+import com.shortener.shortener.repository.ShortenerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShortenerController {
+
+@Autowired
+    ShortenerRepository shortenerRepository;
+
+
+
+}
