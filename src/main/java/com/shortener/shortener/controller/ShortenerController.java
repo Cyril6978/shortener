ff0b3079eab96868b46617f21435de23d4247232
Cyril6978 <cyril.david8978@gmail.com>
2023-07-21 16:04:12 +0200

    week-end!

--- src/main/java/com/shortener/shortener/controller/ShortenerController.java
+++ src/main/java/com/shortener/shortener/controller/ShortenerController.java
@@ -1,7 +1,13 @@
 package com.shortener.shortener.controller;
 
+import com.shortener.shortener.repository.ShortenerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShortenerController {
+
+    @Autowired
+    ShortenerRepository shortenerRepository;
+
 }
--- src/main/java/com/shortener/shortener/repository/ShortenerRepository.java
+++ src/main/java/com/shortener/shortener/repository/ShortenerRepository.java
@@ -4,6 +4,9 @@ import com.shortener.shortener.entity.Shortener;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ShortenerRepository extends JpaRepository<Shortener, Long> {
+public interface ShortenerRepository extends JpaRepository<Shortener, UUID> {
+
 }
