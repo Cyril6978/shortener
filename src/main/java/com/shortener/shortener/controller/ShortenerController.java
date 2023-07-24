3c47a2feb77d24d1237908bbbd7e3464d453a2f7
Camille DINET <[camidinet@gmail.com](mailto:camidinet@gmail.com)>
2023-07-24 22:13:37 +0200

    add method to search url real with sort url

--- src/main/java/com/shortener/shortener/controller/ShortenerController.java
+++ src/main/java/com/shortener/shortener/controller/ShortenerController.java
@@ -3,9 +3,10 @@ package com.shortener.shortener.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shortener.shortener.entity.Shortener;
-import com.shortener.shortener.repository.ShortenerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 
+
+import com.shortener.shortener.service.ShortenerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,55 +17,40 @@ import java.util.Base64;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @RestController
 public class ShortenerController {
 
 @Autowired
-
-    ShortenerRepository shortenerRepository;
+private ShortenerService shortenerService;
 
 @PostMapping("")
 public Shortener createUrl(@RequestBody Shortener shortener) throws IOException {
-
-
     shortener.setId(UUID.randomUUID());
-    shortener.setShortId(generateShortId());
-
+    shortener.setShortId(shortenerService.generateShortId());
     File file = new File("src/main/resources/links.json");
     ObjectMapper objectMapper = new ObjectMapper();
     List<Shortener> myDataList = objectMapper.readValue(file, new TypeReference<List<Shortener>>() {});
-
     for(int i = 0; i < myDataList.size(); i++){
         if(myDataList.get(i).getShortId()==shortener.getShortId()){
-            shortener.setShortId(generateShortId());
+            shortener.setShortId(shortenerService.generateShortId());
             i = -1;
         }
     }
     myDataList.add(shortener);
     objectMapper.writeValue(file, myDataList);
-
     return shortener;
 }
-
-public String generateShortId(){
-    String str="";
-    Random rand = new Random();
-    String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int longueur = alphabet.length();
-    for(int i = 0; i < 8; i++) {
-        int k = rand.nextInt(longueur);
-        str = str + alphabet.charAt(k);
-    }
-    return str;
-//    Base64.Encoder encoder = Base64.getUrlEncoder();
-//    SecureRandom random = new SecureRandom();
-//    byte[] array = new byte[6]; // length is bounded 8
-//    random.nextBytes(array);
-//    return encoder.encodeToString(array);
-
-
+@GetMapping("/{shortId}")
+    public String getOriginalUrl(@PathVariable String shortId) throws IOException{
+    File file = new File("src/main/resources/links.json");
+    ObjectMapper objectMapper = new ObjectMapper();
+    List<Shortener> myDataList = objectMapper.readValue(file, new TypeReference<List<Shortener>>() {});
+    String shortenerToDisplay =  myDataList.stream().filter(
+            myObj -> myObj.getShortId().equals(shortId)
+    ).findFirst().get().getRealUrl();
+return shortenerToDisplay;
 }
-
 
 }
