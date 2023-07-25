888356d6f01727892243e040b53bece978efb080
Camille DINET <[camidinet@gmail.com](mailto:camidinet@gmail.com)>
2023-07-25 09:55:14 +0200

    change method getOriginalUrl

--- src/main/java/com/shortener/shortener/controller/ShortenerController.java
+++ src/main/java/com/shortener/shortener/controller/ShortenerController.java
@@ -3,15 +3,18 @@ package com.shortener.shortener.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shortener.shortener.entity.Shortener;
-
-
 import com.shortener.shortener.service.ShortenerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+
+
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.List;
@@ -43,14 +46,18 @@ public Shortener createUrl(@RequestBody Shortener shortener) throws IOException 
     return shortener;
 }
 @GetMapping("/{shortId}")
-    public String getOriginalUrl(@PathVariable String shortId) throws IOException{
+    public ResponseEntity<String> getOriginalUrl(@PathVariable String shortId) throws IOException{
     File file = new File("src/main/resources/links.json");
     ObjectMapper objectMapper = new ObjectMapper();
     List<Shortener> myDataList = objectMapper.readValue(file, new TypeReference<List<Shortener>>() {});
     String shortenerToDisplay =  myDataList.stream().filter(
             myObj -> myObj.getShortId().equals(shortId)
     ).findFirst().get().getRealUrl();
-return shortenerToDisplay;
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setLocation(URI.create(shortenerToDisplay));
+
+return new ResponseEntity<>(headers,HttpStatus.FOUND);
 }
 
 }
